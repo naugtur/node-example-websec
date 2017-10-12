5a61957e117b9cc3ce911ba7350986983369cba0
Naugtur <naugtur@gmail.com>
2017-10-12 16:49:36 +0200

    naive session implementation

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
+// https://github.com/naugtur/node-example-websec
+
 const express = require('express')
 const cookieParser = require('cookie-parser')
+const uuidV4 = require('uuid/v4')
+const crypto = require('crypto');
 
 const app = express()
+
+const sessionStorage = {}
 
 // public static site
 app.use('/public', express.static('public'))
 
 // login handler
 app.get('/public/login', (req, res) => {
-  res.cookie('sid', '123')
-  res.redirect('/private')
+  const uuid = uuidV4()
+const hash = crypto.createHash('sha256').update(req.query.password).digest('base64');
+
+  if(req.query.login === 'root' && hash === 'jZae727K08KaOmKSgOaGzww/XVqGr/PKEgIMkjrcbJI='){
+    res.cookie('sid', uuid, { maxAge: 900000, httpOnly: true /*, secure: true*/})
+    sessionStorage[uuid]={
+      user: 'root'
+    }
+    setTimeout(()=>{
+      delete sessionStorage[uuid]
+    },20000)
+    res.redirect('/private')
+  } else {
+    res.status(403).redirect('/public')
+  }
 })
 
 // I know we're implementing everything to learn how, but cookie parsing... come on.
@@ -18,11 +37,13 @@ app.use('/private', cookieParser())
 // Our session middleware
 app.use('/private', (req, res, next) => {
   console.log('cookies', req.cookies)
-  if (!req.cookies.sid) {
-    res.status(401).redirect('/public')
-  } else {
+  if (req.cookies.sid && sessionStorage[req.cookies.sid]) {
+    req.session = sessionStorage[req.cookies.sid]
     next()
+  } else {
+    res.status(401).redirect('/public')
   }
+
 })
 
 // The walled garden
