d4601b9d2c684f8cd22788355ffcfb4bf384952f
Naugtur <naugtur@gmail.com>
2017-10-17 21:19:53 +0200

    fix formatting

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
 
 const express = require('express')
 const cookieParser = require('cookie-parser')
-const crypto = require('crypto');
+const crypto = require('crypto')
 const bodyParser = require('body-parser')
 const uuidV4 = require('uuid/v4')
-const db = require('./db') //fake database
+const db = require('./db') // fake database
 
 const app = express()
 
@@ -17,16 +17,16 @@ app.use('/public', express.static('public'))
 // login handler
 app.get('/public/login', (req, res) => {
   const uuid = uuidV4()
-const hash = crypto.createHash('sha256').update(req.query.password).digest('base64');
+  const hash = crypto.createHash('sha256').update(req.query.password).digest('base64')
 
-  if(req.query.login === 'root' && hash === 'jZae727K08KaOmKSgOaGzww/XVqGr/PKEgIMkjrcbJI='){
-    res.cookie('sid', uuid, { maxAge: 900000, httpOnly: true /*, secure: true*/})
-    sessionStorage[uuid]={
+  if (req.query.login === 'root' && hash === 'jZae727K08KaOmKSgOaGzww/XVqGr/PKEgIMkjrcbJI=') {
+    res.cookie('sid', uuid, { maxAge: 900000, httpOnly: true /*, secure: true */})
+    sessionStorage[uuid] = {
       user: 'root'
     }
-    setTimeout(()=>{
+    setTimeout(() => {
       delete sessionStorage[uuid]
-    },20000)
+    }, 20000)
     res.redirect('/private')
   } else {
     res.status(403).redirect('/public')
@@ -45,7 +45,6 @@ app.use('/private', (req, res, next) => {
   } else {
     res.status(401).redirect('/public')
   }
-
 })
 
 app.post('/private/add', bodyParser.urlencoded(), (req, res) => {
