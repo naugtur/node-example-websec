7837fac6e5dce16ee155139576904c192d13cab0
Naugtur <naugtur@gmail.com>
2017-10-17 15:11:04 +0200

    simple hackable form

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const uuidV4 = require('uuid/v4')
+const db = require('./db') //fake database
 
 const app = express()
 
@@ -23,6 +26,17 @@ app.use('/private', (req, res, next) => {
   } else {
     next()
   }
+})
+
+app.post('/private/add', bodyParser.urlencoded(), (req, res) => {
+  db.get('posts')
+    .push({ id: uuidV4(), title: req.body.title})
+    .write()
+  res.redirect('/private/form.html')
+})
+app.get('/private/posts', (req, res) => {
+  res.json(db.get('posts')
+    .value())
 })
 
 // The walled garden
